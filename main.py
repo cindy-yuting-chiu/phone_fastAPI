739b029dd79d9d0e7392414fe1374c2116e97c1e
cindy chiu <yc506@duke.edu>
2021-09-10 21:22:52 +0000

    add new functin

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ async def root():
 
 @app.get("/phone/{num1}")
 async def area_code(num1: str):
-    """Add two numbers together"""
+    """Return state for the given phone number"""
     num = str(num1)
     area = int(num[0:3])
     state = df.loc[df['area_code']== area ,['State']]['State'].unique().tolist()
     city = df.loc[df['area_code']== area ,['City']]['City'].unique().tolist()
     return {'state': state, 'city':city}
+
+@app.get("/state/{st}")
+async def state_code(st : str):
+    """Return area code for a given state """ 
+    area_code  = df.loc[df['State']== st ,['area_code']]['area_code'].unique().tolist()
+    return {'area code': area_code}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
